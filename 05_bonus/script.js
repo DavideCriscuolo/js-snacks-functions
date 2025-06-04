84be5882e148f441a18853c704572ca4c6a16134
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-04 17:41:52 +0200

    creata la funzione bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,26 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+function saluta(nome) {
+  //         anno, mese, giorno, ora, minuto,secondo,ms
+  const data = new Date(); //costruttore è una funzione speciale che crea ogetti
+  let ora = data.getHours();
+  if (ora <= 13) {
+    return `Buongiorno ${nome}`;
+  } else if (ora <= 17) {
+    return `Buon pomeriggio ${nome}`;
+  } else if (ora >= 17) {
+    return `Buona sera ${nome}`;
+  } else {
+    return "Ciao";
+  }
 
+  return ``;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(saluta(name));
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
